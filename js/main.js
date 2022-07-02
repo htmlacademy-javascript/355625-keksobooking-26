dc2f1e1cb4c48e5986be205b074ea2d951f9e32c
Maxmitab <maxpavlenko@hotmail.com>
2022-07-02 19:23:59 +0300

    Исправил 2 ошибки: Strings must use singlequote и Missing semicolon

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ const zeroAdd = (num) => {
     const numZeroes = places - num.toString().length + 1;
     return Array(+numZeroes).join("0") + num;
   }
-  return num
+  return num;
 };
 
 /*
@@ -91,7 +91,7 @@ const offer = {
   'checkin': ['12:00','13:00','14:00'],
   'checkout': ['12:00','13:00','14:00'],
   'features': ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-  'description': 'Дом премиум-класса "Поклонная 9" расположен в исторически значимом и одном из самых престижных районов столицы  Дорогомилово, перпендикулярно Кутузовскому проспекту. Слева от дома "Поклонная 9" находится мемориальный комплекс Парк Победы и знаменитая Триумфальная арка. Добраться до Кремлевской набережной от жилого комплекса можно всего за 10-15 минут, а выезд на ТТК расположен в 900 м.',
+  'description': `Дом премиум-класса "Поклонная 9" расположен в исторически значимом и одном из самых престижных районов столицы  Дорогомилово, перпендикулярно Кутузовскому проспекту. Слева от дома "Поклонная 9" находится мемориальный комплекс Парк Победы и знаменитая Триумфальная арка. Добраться до Кремлевской набережной от жилого комплекса можно всего за 10-15 минут, а выезд на ТТК расположен в 900 м.`,
   'photos': ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg']
 };
 const location = {
