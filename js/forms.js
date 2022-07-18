55d898f9bd96c6d0f0189fed32737e023fdb1f01
Maxmitab <maxpavlenko@hotmail.com>
2022-07-19 01:07:52 +0300

    Сдача 8-го задания 1-ой части

--- js/forms.js
+++ js/forms.js
@@ -1,22 +1,61 @@
-
-
-const disableForm = (clasContainer, classAdd, toggleName) => {
-  const container = document.querySelector(clasContainer);
-  const formElement = container.querySelector('form');
-  const fieldsetElements = container.querySelectorAll('fieldset');
-  const selectElements = container.querySelectorAll('select');
+const switchForm = (classAdd, toggleName, index) => {
+  const allForms = document.forms;
   if (classAdd) {
-    formElement.classList.add(classAdd);
-  }
-  if (toggleName) {
-    for (let elem of fieldsetElements) {
-      elem.setAttribute(toggleName, '');
-    }
-    for (let elem of selectElements) {
-      elem.setAttribute(toggleName, '');
+    for (let elem of allForms) {
+      elem.classList.add(classAdd);
     }
   }
-  return container;
+  if (toggleName && index <= allForms.length) {
+    for (let elem of allForms[index].elements) {
+      elem.setAttribute(toggleName, '');
+    }
+  }
+  return allForms;
 };
 
-export {disableForm};
+// Валидация формы
+
+const sendForm = document.querySelector('.ad-form');
+const pristine = new Pristine(sendForm, {
+  classTo: 'ad-form__element',
+  errorTextParent: 'ad-form__element',
+});
+
+const roomsField = sendForm.querySelector('[name="rooms"]');
+const capacityField = sendForm.querySelector('[name="capacity"]');
+const guestsOption = {
+  1: 'это только для 1 комнаты',
+  2: 'это только для 2 комнат',
+  3: 'это только для 3 комнат',
+  100: 'не для гостей',
+};
+
+
+const validateNicknameTitle = (value) => {
+  return value.length >= 30 && value.length <= 100;
+};
+const validateNicknamePrice = (value) => {
+  return parseInt(value) <= 100000 && parseInt(value) > 0;
+};
+
+const validateGuestOption = () => {
+  return roomsField.value >= capacityField.value;
+};
+
+function getDeliveryErrorMessage() {
+  return `${guestsOption[capacityField.value]}`;
+}
+
+pristine.addValidator(sendForm.querySelector('#title'), validateNicknameTitle, 'должно быть от 30 до 100 символов');
+pristine.addValidator(sendForm.querySelector('#price'), validateNicknamePrice, 'должно быть от 1 до 100 000 рублей');
+pristine.addValidator(capacityField, validateGuestOption, getDeliveryErrorMessage);
+
+const validateForm = () => {
+  sendForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    pristine.validate();
+  });
+};
+
+
+export {switchForm, validateForm};
--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 import {createOffersArray} from './data.js';
 import {renderCards} from './templates.js';
-import {disableForm} from './forms.js';
+import {switchForm,validateForm} from './forms.js';
 
-disableForm('.map__filters-container', 'ad-form--disabled', 'disabled');
-disableForm('.notice', 'ad-form--disabled', 'disabled');
+
+// class = 'ad-form--disabled'
+switchForm('','disabled',1);
+
 
 const mapCanvas = document.querySelector('#map-canvas');
 const objArr = createOffersArray(10);
 const templateFull = renderCards(objArr[0]);
 mapCanvas.append(templateFull);
 
-
+validateForm();
 
 
 
